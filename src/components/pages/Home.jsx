7ac900e210e1d5f43f60442029ddf919c4e011e4
sholabioku <bilushonline@gmail.com>
2022-05-02 17:57:32 +0100

    Add UserList component

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import UserResults from '../users/UserResults';
 
 const Home = () => {
   return (
-    <div>
-      <h1 className='text-6xl'>Welcome</h1>
-    </div>
+    <>
+      {/*SEARCH COMPONENT*/}
+      <UserResults />
+    </>
   );
 };
 
