28cd0c8826f03f6621a58c600aa26d523f8d9d1e
sholabioku <bilushonline@gmail.com>
2022-05-03 17:07:55 +0100

    Add alert-context-reducer setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
+import { AlertProvider } from './context/alert/AlertContext';
 import { GithubProvider } from './context/github/GithubContext';
 import About from './pages/About';
 import Home from './pages/Home';
@@ -9,20 +10,22 @@ import NotFound from './pages/NotFound';
 function App() {
   return (
     <GithubProvider>
-      <Router>
-        <div className='flex flex-col justify-between h-screen'>
-          <Navbar />
-          <main className='container mx-auto px-3 pb-12'>
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/about' element={<About />} />
-              <Route path='/notFound' element={<NotFound />} />
-              <Route path='/*' element={<NotFound />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
+      <AlertProvider>
+        <Router>
+          <div className='flex flex-col justify-between h-screen'>
+            <Navbar />
+            <main className='container mx-auto px-3 pb-12'>
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/notFound' element={<NotFound />} />
+                <Route path='/*' element={<NotFound />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </Router>
+      </AlertProvider>
     </GithubProvider>
   );
 }
